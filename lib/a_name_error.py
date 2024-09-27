4197ac9864eaec7545e50ce531f07c64751a9926
Melvin jerry <+>
2024-09-27 13:57:16 +0300

    funky

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
-print("hello_world")
+# print("hello_world")
+def greet_programmer():
+    print ('Hello,programmer')
+
+greet_programmer()
+
+def greet(name):
+    print(f"Hello,{name}")
+
+greet("Jerry")
+
+def greet_with_default(name='programmer'):
+    print(f'Hello,{name}')
+greet_with_default('Ambwere')    
+
+def add(sum1,sum2):
+    print (sum1 + sum2)
+add(2,3)
+
+def halve(num):
+    print (num/2)
+halve(100)
